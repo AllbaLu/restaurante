53ce5ada59e639281db7dffa9cab9d6279484fa2
albal <albaluccia@gmail.com>
2025-12-04 13:51:09 +0800

    changes- packages, vite.config are being changed to /restaurant

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
 
 export default defineConfig({
+  root: 'frontend',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   plugins: [
     react({
       babel: {
@@ -10,3 +17,4 @@ export default defineConfig({
     }),
   ],
 })
+
